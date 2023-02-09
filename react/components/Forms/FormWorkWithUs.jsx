3c0fbe4435a191e4eabaf3b830b59e33dac2bbc9
ogonzalez-webimpacto <122329660+ogonzalez-webimpacto@users.noreply.github.com>
2023-02-09 11:50:57 -0600

    Form with bootstrap

--- react/components/Forms/FormWorkWithUs.jsx
+++ react/components/Forms/FormWorkWithUs.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import '../../css/index.css'
-import { useState } from 'react';
+import React,{ Component,useState } from 'react';
+import '../../css/formWorkWithUs.css'
+import '../../css/bootstrap.css';
+
 
 function FormWorkWithUs(){
-  const [nombre,setNombre] = useState="";
-  const [apellidos,setApellidos] = useState="";
-  const [email,setEmail]= useState="";
-  const [poblacion,setPoblacion] = useState="";
-  const [provincia,setProvincia]= useState="";
-  const [cp,setCP]= useState="";
-  const [tel1,setTel1] = useState="";
-  const [tel2,setTel2] =useState ="";
+
+  const [nombre,setNombre] = useState("");
+  const [apellidos,setApellidos] = useState("");
+  const [email,setEmail]= useState("");
+  const [poblacion,setPoblacion] = useState("");
+  const [provincia,setProvincia]= useState("");
+  const [cp,setCP]= useState("");
+  const [tel1,setTel1] = useState("");
+  const [tel2,setTel2] = useState("");
   const [message, setMessage] = useState("");
 
   let handleSubmit = async (e) => {
@@ -43,98 +45,90 @@ function FormWorkWithUs(){
     }
   };
 
-
   return(
     <>
     <div className="formbold-main-wrapper">
-      <div className="formbold-form-wrapper">
-        <form onSubmit={handleSubmit}>
-        <div className="message">{message ? <p>{message}</p> : null}</div>
-          <div className="formbold-mb-5">
-            <label for="nombre" className="formbold-form-label"> Nombre </label>
-            <input
-              type="text"
-              value={nombre}
-              name="nombre"
-              id="nombre"
-              placeholder="Nombre"
-              className="formbold-form-input"
-            />
-          </div>
-          <div className="formbold-mb-5">
-            <label for="apellidos" className="formbold-form-label"> Apellidos </label>
-            <input
-              type="text"
-              value={apellidos}
-              name="apellidos"
-              id="apellidos"
-              placeholder="Apellidos"
-              className="formbold-form-input"
-            />
-          </div>
+    <div className="formbold-form-wrapper">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-2">
+          <input
+            type="text"
+            value={nombre}
+            name="nombre"
+            id="nombre"
+            placeholder="Nombre"
+            className="formbold-form-input"
+          />
+        </div>
+        <div className="mb-2">
+          <input
+            type="text"
+            value={apellidos}
+            name="apellidos"
+            id="apellidos"
+            placeholder="Apellidos"
+            className="form-input"
+          />
+        </div>
 
-          <div className="formbold-mb-5">
-            <label for="email" className="formbold-form-label"> Email </label>
-            <input
-              type="email"
-              value={email}
-              name="email"
-              id="email"
-              placeholder="Correo electrónico"
-              className="formbold-form-input"
-            />
-          </div>
+        <div className="mb-2">
+          <input
+            type="email"
+            value={email}
+            name="email"
+            id="email"
+            placeholder="Correo electrónico"
+            className="form-input"
+          />
+        </div>
 
-          <div className="formbold-mb-5">
-            <label for="poblacion" className="formbold-form-label"> Población </label>
-            <input
-              type="text"
-              name="poblacion"
-              id="poblacion"
-              placeholder="Población"
-              className="formbold-form-input"
-            />
-          </div>
+        <div className="mb-2">
+          <input
+            type="text"
+            name="poblacion"
+            id="poblacion"
+            placeholder="Población"
+            className="formbold-form-input"
+          />
+        </div>
 
-          <div className="formbold-mb-5">
-            <label for="cp" className="formbold-form-label"> Codigo Postal </label>
-            <input
-              type="text"
-              name="cp"
-              id="cp"
-              placeholder="Codigo Postal"
-              className="formbold-form-input"
-            />
-          </div>
+        <div className="mb-2">
+          <input
+            type="text"
+            name="cp"
+            id="cp"
+            placeholder="Codigo Postal"
+            className="formbold-form-input"
+          />
+        </div>
 
-          <div className="formbold-mb-5">
-            <label for="tel1" className="formbold-form-label"> Telefono 1 </label>
-            <input
-              type="text"
-              name="tel1"
-              id="tel1"
-              placeholder="Telefono 1"
-              className="formbold-form-input"
-            />
-          </div>
+        <div className="mb-2">
+          <input
+            type="text"
+            name="tel1"
+            id="tel1"
+            placeholder="Telefono 1"
+            className="formbold-form-input"
+          />
+        </div>
 
-          <div className="formbold-mb-5">
-            <label for="tel2" className="formbold-form-label"> Telefono 2 </label>
-            <input
-              type="text"
-              name="tel2"
-              id="tel2"
-              placeholder="Telefono 2"
-              className="formbold-form-input"
-            />
-          </div>
-          <div>
-            <button className="formbold-btn">Enviar</button>
-          </div>
-        </form>
-      </div>
+        <div className="mb-2">
+
+          <input
+            type="text"
+            name="tel2"
+            id="tel2"
+            placeholder="Telefono 2"
+            className="formbold-form-input"
+          />
+        </div>
+        <div>
+          <button className="btn btn-primary">Enviar</button>
+        </div>
+      </form>
     </div>
-    </>
+  </div>
+  </>
   )
 }
 
